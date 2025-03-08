dd43c893f1e18ea1e310a65a777a7a62c7d490d6
chemimartinez77 <chemimartinez@gmail.com>
2025-03-08 10:13:45 +0100

    Funcionando. Seguimos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
-//tailwind.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: 'var(--background-color)',
+        foreground: 'var(--foreground-color)',
+      },
+    },
   },
   plugins: [],
-};
+}
